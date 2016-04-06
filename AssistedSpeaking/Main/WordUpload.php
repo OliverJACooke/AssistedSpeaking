dcc915d75dc0198478190b39da6e00892baa9de0
OliverJACooke <ocooke@hotmail.com>
2016-04-06 12:20:37 +0100

    Added additional criteria for word groups

--- AssistedSpeaking/Main/WordUpload.php
+++ AssistedSpeaking/Main/WordUpload.php
@@ -1,21 +1,27 @@
 <?php
-
+			
+	
 	// Check connection
 	if (!$con) {
     	die("Connection failed: " . mysqli_connect_error());
 	}
 	
 	if(isset($_POST['submit'])) {
-	
+		$group = $_POST['group'];
 		$word = $_POST['word'];
 		$phrase = $_POST['phrase'];
+		$image = $_POST['image'];
 		
-		$sql = "INSERT INTO Word 
-						(Word,
-						Phrase)
+		$sql = "INSERT INTO Words 
+						(GroupID,
+						PhraseName,
+						Phrase,
+						Image)
 					VALUES (
+						'".$group."',
 						'".$word."', 
-						'".$phrase."')";
+						'".$phrase."',
+						'../Images/WordImages/".$image.".png')";
 						
 			
 			if (mysqli_query($con, $sql)) {
@@ -37,12 +43,18 @@
 </br>n
 	<a href="../Main/Default.php">Back to AssistedSpeaking</a>
         <form action="../Main/WordUpload.php" method="post">
+			<h1>Group</h1>
+        	<p>Group ID of 0 for main words</p>
+        	<input type="text" name="group" id="group"/>
         	<h1>Word</h1>
         	<p>This is the word that will be displayed within the buttons</p>
         	<input type="text" name="word" id="word"/>
         	<h1>Phrase</h1>
         	<p>This is the sentence that will be read by the systems</p>
         	<input type="text" name="phrase" id="phrase"/>
+			<h1>Image Name</h1>
+			<p>Image name, with no spaces</p>
+        	<input type="text" name="image" id="image"/>
         	</br></br>
         	<i>All I will say is be gentle...</i>
         	<input type="submit" name="submit" value="submit">
