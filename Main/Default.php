ceca2b371dce1a1e4d68f3e9cd91d9ecb2ef9581
oliverjacooke <ocooke@hotmail.com>
2016-04-11 21:27:58 +0100

    Added caching manifest

--- Main/Default.php
+++ Main/Default.php
@@ -12,7 +12,7 @@
 	}
 ?>
 
-<html><!-- manifest="../cache.manifest">-->
+<html manifest="../cache.manifest">
 	<head>
 		<meta charset="utf-8">
   		<meta name="viewport" content="width=device-width, initial-scale=1">
