c672b2dc4499843cf406a70e01654734eb8e7f7f
oliverjacooke <ocooke@hotmail.com>
2016-04-11 20:57:14 +0100

    Changed home page layout

--- Main/Home.php
+++ Main/Home.php
@@ -1,8 +1,10 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <title>Words</title>
+	
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+	<link href="../App_Themes/StyleSheet.css" rel="stylesheet" /> <!-- Custom Style Sheet-->
 </head>
     
 <body>
@@ -15,7 +17,7 @@
 	<div class="row">
 		<div class="col-lg-offset-2 col-lg-8" style="padding-top:50px;">
 			<h1 style="color:black;">
-				The Bespoke Online Assistive Speaking Application
+				THE BESPOKE ONLINE ASSISTIVE SPEAKING APPLICATION
 			</h1>
 		</div>
 	</div>
@@ -38,92 +40,141 @@
 			</div>
 		</div>
 	</nav>
-
-<div class="container" style="height:2000px;">
-	<div class="row">
-		<h3>About Us</h3>
-		<p>
-			YouSpeak is a project started by 3 students as part of a module at university to produce a bespoke AAC application based on perticular users needs. YouSpeak goes beyond the orignal scope with a completly
-			customisable package that can be change based on any users needs. YouSpeak is a currently accessible and usable by anyone, however it is an early alpha product so is still under development, the core services are there but there is so
-			we want to make this solution so much more.
-		</p>
-		<p> 
-			YouSpeak is different to current AAC applications on the market as it is an online solution, avaliable to use in an offline environment. This means it comes with a heap of functionality that traditional AAC applications
-			just dont have. 
-		</p>
-	</div>
-	<div class="row">
-		<h3>Key Features</h3>
-	</div>
-	</br></br></br>
-	<div class="row col-centered">
-		<div class="col-lg-4">
-			<h2>
-				Use YouSpeak Anyware
-			</h2>
-		</div>
-		<div class="col-lg-5">
-			<p>
-				YouSpeak uses cutting edge technology to deliever its service offline. With the use of HTML5 data caching means you can login once and the application just keeps on working.
-				Even if you shut your device down!
-			</p>
-		</div>
-		<div class="col-lg-3">
-			<img class="img-responsive" src="../images/HTML5.png" alt="HTML5 Logo"/>
+<div class="homeContainer">
+	<div class="container">
+		<div class="row" style="text-align:center;" >
+			<div class="col-lg-offset-4 col-lg-4">
+				<h1>ABOUT US</h1>
+				</br> 
+			</div>
+			<div class="col-lg-12">
+				<p>
+					YouSpeak is a project started by 3 students as part of a module at university to produce a bespoke AAC application based on perticular users needs. YouSpeak goes beyond the orignal scope with a completly
+					customisable package that can be change based on any users needs. YouSpeak is a currently accessible and usable by anyone, however it is an early alpha product so is still under development, the core services are there but there is so
+					we want to make this solution so much more.
+				</p>
+				<p> 
+					YouSpeak is different to current AAC applications on the market as it is an online solution, avaliable to use in an offline environment. This means it comes with a heap of functionality that traditional AAC applications
+					just dont have. 
+				</p>
+			</div>
 		</div>
 	</div>
-	</br></br></br></br></br></br>
-	<div class="row col-centered">
-		<div class="col-lg-3">
-			<img class="img-responsive" src="../images/scanning.png" alt="Scanning"/>
+</div>
+<div class="homeContainer" style="background-color:#1F242A; color:white;">
+	<div class="container">
+		<div class="row" style="text-align:center;">
+			<div class="col-lg-offset-4 col-lg-4">
+				<h1> 
+					HOW IT WORKS
+				</h1>
+			</div>
 		</div>
-		<div class="col-lg-5">
-			<p>
-				YouSpeak does not just only work with a mouse or touch screen, with YouSpeaks single step scanning implemented to the system means that uses with limited mobility in there hands
-				will be able to use the solution
-			</p>
+		<div class="row">
+			<div class="col-lg-offset-4 col-lg-4" style="text-align:center; color:#777">
+				<h2> 
+					OUR SIMPLE SYSTEM MEANS YOU CAN GET UP AND RUNNING IN THREE EASY STEPS
+				</h2>
+				</br>
+				</br>
+				
+			</div>
+			
 		</div>
-		<div class="col-lg-4">
-			<h2>
-				Bespoke Interaction
-			</h2>
+		<div class="row" style="text-align:center;">
+			<div class="col-lg-3">
+				<h3>Register</h3>
+				<p>
+					Create your own customisable account
+				</p>
+			</div>
+			<div class="col-lg-1">
+				<h3>></h3>
+			</div>
+			<div class="col-lg-3">
+				<h3>Configure</h3>
+				<p>
+					Change YouSpeaks settings to suit your needs
+				</p>
+			</div>
+			<div class="col-lg-1">
+				<h3>></h3>
+			</div>
+			<div class="col-lg-3">
+				<h3>Go!</h3>
+				<p>
+					Start using our application
+				</p>
+			</div>
+			<div class="col-lg-1" style="text-align:center;">
+				<h3>Simple!</h3>
+			</div>
 		</div>
 	</div>
-	</br></br></br></br></br></br>
-	<div class="row col-centered">
-		<div class="col-lg-4">
-			<h2>
-				Customisable Look and Feel
-			</h2>
+</div>
+</br></br></br>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-offset-4 col-lg-4">
+				<h1>Why YouSpeak</h1>
+			</div>
 		</div>
-		<div class="col-lg-5">
-			<p>
-				YouSpeak has an advanced setting menu that is only getting bigger! Change the colour of your YouSpeak with a custom colour theme, background colour and scanning. With the plan 
-				of adding layout changes in the future to make your experience the best it can be.
-			</p>
+		</br> </br> </br>
+		<div class="row">
+			<div class="col-lg-2">
+				<img class="img-responsive" src="../images/HTML5.png" alt="HTML5 Logo"/>
+			</div>
+			<div class="col-lg-4">
+				<h4>
+					Use YouSpeak Anyware
+				</h4>
+				<p>
+					YouSpeak uses cutting edge technology to deliever its service offline. With the use of HTML5 data caching means you can login once and the application just keeps on working.
+					Even if you shut your device down!
+				</p>
+			</div>
+			
+			<div class="col-lg-2">
+				<img class="img-responsive" src="../images/scanning.png" alt="Scanning"/>
+			</div>
+			<div class="col-lg-4">
+				<h4>
+					Bespoke Interaction
+				</h4>
+				<p>
+					YouSpeak does not just only work with a mouse or touch screen, with YouSpeaks single step scanning implemented to the system means that uses with limited mobility in there hands
+					will be able to use the solution
+				</p>
+			</div>
 		</div>
-		<div class="col-lg-3">
-			<img class="img-responsive" src="../images/colourChange.png" alt="Colour Change"/>
-		</div>
-	</div>
-	</br></br></br></br></br></br>
-	<div class="row col-centered">
-		<div class="col-lg-3">
-			<img class="img-responsive" src="../images/NewWords.png" alt="Scanning"/>
-		</div>
-		<div class="col-lg-5">
-			<p>
-				You can completly generate your own word banks using YouSpeak, decide what you would like to say and put it in to YouSpeak on the fly. This is because of YouSpeak's use
-				of a powerful and realistic sounding text to speach engine.
-			</p>
-		</div>
-		<div class="col-lg-4">
-			<h2>
-				Custom Word Banks
-			</h2>
-		</div>
-	</div>
-	  
+		</br></br></br></br></br></br>
+		<div class="row col-centered">
+				<div class="col-lg-2">
+				<img class="img-responsive" src="../images/colourChange.png" alt="HTML5 Logo"/>
+			</div>
+			<div class="col-lg-4">
+				<h4>
+					Customisable Look and Feel
+				</h4>
+				<p>
+					YouSpeak has an advanced setting menu that is only getting bigger! Change the colour of your YouSpeak with a custom colour theme, background colour and scanning. With the plan 
+					of adding layout changes in the future to make your experience the best it can be.
+				</p>
+			</div>
+			
+			<div class="col-lg-2">
+				<img class="img-responsive" src="../images/NewWords.png" alt="Scanning"/>
+			</div>
+			<div class="col-lg-4">
+				<h4>
+					Custom Word Banks
+				</h4>
+				<p>
+					You can completly generate your own word banks using YouSpeak, decide what you would like to say and put it in to YouSpeak on the fly. This is because of YouSpeak's use
+					of a powerful and realistic sounding text to speach engine.
+				</p>
+			</div>
+	</div>  
 </div>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 	<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
