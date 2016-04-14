fa71aed6b2511d026b4a507d362ca4f5ee608d07
Matt <msuttong@honeycomb.co.uk>
2016-04-14 18:05:48 +0100

    Removed changes to allow for push

--- CodeBehind/database.php
+++ CodeBehind/database.php
@@ -1,8 +1,7 @@
 <?php
 	function db_connect_mysqli(){
 		// Function to establish initial connection to the database
-        	//$con = mysqli_connect("188.121.44.165","AssistedSpeak","a55!sT3D","AssistedSpeak"); //Connection string	
-        	$con = mysqli_connect("127.0.0.1","root","","AssistedSpeak"); //Connection string	
+        	$con = mysqli_connect("188.121.44.165","AssistedSpeak","a55!sT3D","AssistedSpeak"); //Connection string	
 		// Check connection
 		if (!$con) {
     			die("Connection failed: " . mysqli_connect_error());
