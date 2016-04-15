12d719678004b8ad7495776094f26a5d82911310
Matt <msuttong@honeycomb.co.uk>
2016-04-15 19:23:39 +0100

    Added content to Guides.php. Still need to add more for configuration. Also added another few images to the images repo to be used by guides.php

--- Main/Guides.php
+++ Main/Guides.php
@@ -1,6 +1,6 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <title>Words</title>
+    <title>Guides DashBoard</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 </head>
@@ -29,14 +29,32 @@
 		</div>
 	</nav>
 	<div class="container">
-		<h1>Guides</h1>
+		<h1><b>Guides</b></h1><hr><hr>
+		<p> Welcome to YouSpeak's Guide Dashboard. If you've found yourself here then you're probably in need of help, and that's exactly what this area of the application aims to do. Wihtin this section, you'll find information on how to general and advanced use of YouSpeak, as well as the information necessary to configure the application should you require it.</p><hr><hr>
+		<h2>Application Basics</h2>
+		<p>The Main Page, and primary function, of the YouSpeak application is the Speech Tool. This is where you'll spend most of your time, so it's a good idea to have an understanding of how it works, and how to use it.</p><hr><hr>
+		<h3>Navigation and Speech Bar</h3>
+
+		<p>At the top of the tool you'll see the navigation bar. From this bar you can access the Settings, as well as the Dashboard of the application, amongst other pages. Additional links may appear as the functionality the application increases, so watch this space for future updates!</p><br>
+		<p>The area below the naviagtion bar is the speech bar. Providing that the application is set to <i>Push to Talk</i>, each selected word will appear in this location to create a phrase. More information will be provided on configuration later in this guide.</p><br>
+		<img class="img-responsive" src="../images/nav_bar_example.png" alt="Navigation Bar"/><hr><hr>
+
+<h3>Speech Command, Deletion and Settings</h3>
+
+		<p>Towards the top right of the speech application page, you can see three icons. The topmost Cog icon provides access to the application settings, which will be discussed at a later point. Below this, two icons can be seen. The bin icon can be used to clear the speech bar of words, whilst the speech icon will speak the current phrase.<p><br>
+		<img class="img-responsive" src="../images/speech_button_example.png" alt="Speech Area"/><hr><hr>
+
+		<h3>Word Bank</h3>
+
+		<p>The most important area of the speech application page is the word bank, which contains all the words and categories you'll use everyday to speak. This area can found below both the navigation and speech bar.</p>
+
+		<p>The Word Bank is split into words and categories. The two can be distinguished easily, as categories are surrounded by a black box whilst words are not, and consequently have no border.</p><br>
+
+		<img class="img-responsive" src="../images/word_bank_example.png" alt="Speech Area"/><hr><hr>
 		
-		<!--
+		<h2>Application Configuration and Adjustment</h2>
+		</p>YouSpeak provides you with a number of configuration options to adjust and change the application should you need to. We hope the application is suited to your needs, but we also realise that your needs may change and vary on a regular basis.</p>
 		
-			Add content here
-		
-		-->
-	
 	</div>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 	<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
